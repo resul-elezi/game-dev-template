659b7a5583ce857086b4e42b21e535cacbf5dbe1
Resul Elezi <resul_elezi@protonmail.com>
2026-07-10 21:37:28 +0200

    defined ctx variable

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 'use strict';
 
 const canvas = document. getElementById('game-screen');
+const ctx = canvas.getContext('2d');
+
+console.log('Context: ', ctx);
 
 class Game {
     constructor() {}
