ccad7a01d47524e8995ab2456ac5f7dd84af1254
Resul Elezi <resul_elezi@protonmail.com>
2026-07-30 08:18:03 +0200

    added System class

---
+++ classes/System.js
@@ -0,0 +1,24 @@
+class System {
+    constructor(systemType) {
+        this.systemType = systemType;
+        this.entities = [];
+    }
+}
+
+class MovementSystem extends System {
+    constructor(systemType) {
+        super(systemType);
+        this.componentRequirements = ['Movement', 'Position'];
+    }
+
+    update = () => {
+        for (let i = 0; i < this.entities.length; i++) {
+            const entity = this.entities[i];
+
+            let { Movement, Position } = this.entities.components;
+
+            Position.x += Movement.vX;
+            Position.y += Movement.vY;
+        }
+    }
+}
